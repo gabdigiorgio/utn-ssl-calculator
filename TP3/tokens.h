648dfb92a77e30266c453095b9f1dd0c9b5f6dc6
Agustin <102826488+AgusNeus@users.noreply.github.com>
2022-11-13 23:48:07 -0300

    Update tokens.h

--- TP3/tokens.h
+++ TP3/tokens.h
@@ -1,26 +1,13 @@
 enum token{
-
 	FDT,
-
 	NL,
-
 	VAR,
-
 	CTE,
-
 	IDENTIFICADOR,
-
 	CONSTANTE_REAL,
-
 	OPERADOR_ASIGNACION,
-
 	CARACTER_SIMPLE,
-
 	SALIR
-
 };
 
-
-
 typedef enum token TOKEN;
-
